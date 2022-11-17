7364900f340522d88f3d2f8531dd0170557e7e85
Elarh Adu <elarhadu@gmail.com>
2022-11-17 19:52:48 +0000

    Fix linters errors

--- script.js
+++ script.js
@@ -10,19 +10,16 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   navMenu.classList.remove('active');
 }));
 
-
 // Form Validation
-const form = document.querySelector("#form")
-const email = document.querySelector("#email")
-const error = document.getElementById("error")
+const form = document.querySelector('#form');
+const email = document.querySelector('#email');
+const error = document.getElementById('error');
 
 form.addEventListener('submit', (e) => {
-
   e.preventDefault();
   if (email.value !== email.value.toLowerCase() || email.value === '') {
     error.textContent = 'Please enter email in lower case';
     return;
   }
-
   form.submit();
-})
+});
