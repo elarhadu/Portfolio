a52a5f86b8893724b2b22d3334b09d78a9c91f36
Elarh Adu <elarhadu@gmail.com>
2022-11-19 14:43:39 +0000

    Modify js file

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 /* eslint-disable linebreak-style */
 const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
+const navMenu = document.querySelector('.desktop-navigation');
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
+  header.classList.toggle('full-screen');
+  body.classList.toggle('no-scroll');
 });
 document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
+  header.classList.remove('full-screen');
+  body.classList.remove('no-scroll');
 }));
-
-// <!-- Modal View section  -->
-const modal = document.querySelector('#modal');
 
 const projectCard = [
   {
@@ -81,26 +85,30 @@ const projectCard = [
   },
 ];
 
+const modal = document.querySelector('#modal');
+
 // eslint-disable-next-line no-return-assign
 projectCard.map((card, index) => document
-  .getElementById('Portfolio').innerHTML += ` 
-  <div class="card" id='card-${index}'>
-  <img src="${card.mainImage}" alt="card-img" />
-  <div class="proj-content">
-    <h3 id="card-title" tabindex="0">${card.projectTitle}</h3>
-    <ul>
-    ${card.technology.map((tech) => `<li class="text">${tech}</li>
+  .getElementById('desktop-portfolio').innerHTML += `
+    <article class="container2 desktop-project-1" id='card-${index}'>
 
-    `).join('')}
-    </ul>
-    <div class="project-btn" tabindex="0">
-      <a href="javascript:openModal(${index})" class="text-btn">See this project</a>
-      <img src="imgs/Union.svg" class="img-btn" alt="right-arrow" />
-      <img src="imgs/Union-mobil.png" class="img-btn-mob" alt="right-arrow" />
-    </div>
-  </div>
-</div>
-  `);
+        <img class="images img-${index}" src="${card.mainImage}" alt="A deccorative laptop background">
+        <div class="description-container desc-container-${index}">
+          <h3 class="project-name project-name-${index}">${card.projectTitle}</h3>
+            <div id="language-container" class="language-container-${index}">
+            ${card.technology.map((tech) => `
+            <div class="languages">
+              <div class="text">${tech}</div>
+            </div>
+            `)}
+            </div>
+          <div class="link-container see-more">
+            <div class="see-this-project see-this-project-${index}">See this project</div>
+            <div class="arrow modal-arrow-${index}"><a href="javascript:openModal(${index})">&#x2192</a></div>    
+          </div>
+        </div>
+      </article>
+    `);
 
 function openModal(num = null) {
   if (num != null) {
@@ -124,14 +132,62 @@ function openModal(num = null) {
     document.getElementById('modal-project-source').href = projectCard[num].linkToSource;
 
     modal.classList.add('active');
+    body.classList.add('no-scroll');
   }
 }
 
 function closeModal(index = null) {
   if (index != null) {
     modal.classList.remove('active');
+    body.classList.remove('no-scroll');
   }
 }
 
 openModal();
 closeModal();
+
+// Form Validation With JavaScript
+const form = document.querySelector('#form');
+const email = document.querySelector('#email');
+const error = document.getElementById('error');
+const username = document.querySelector('#user-name');
+const message = document.querySelector('#message');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (email.value !== email.value.toLowerCase() || email.value === '') {
+    error.textContent = 'Please enter email in lower case';
+    return;
+  }
+
+  form.submit();
+});
+
+// Local storage for form input
+
+function storeData() {
+  const formData = {
+    username: username.value,
+    email: email.value,
+    message: message.value,
+  };
+
+  localStorage.setItem('formData', JSON.stringify(formData));
+}
+
+function getData() {
+  if (!localStorage.getItem('formData')) {
+    storeData();
+  }
+
+  const localData = JSON.parse(localStorage.getItem('formData'));
+  username.setAttribute('value', localData.username);
+  email.setAttribute('value', localData.email);
+  message.textContent = localData.message;
+}
+
+getData();
+
+username.addEventListener('change', storeData);
+email.addEventListener('change', storeData);
+message.addEventListener('change', storeData);
